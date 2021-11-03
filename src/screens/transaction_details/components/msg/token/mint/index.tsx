e5393d53fdfe9f7756e2f9f63d2be2bccd295e0e
chocoSprite <chocosprite21@gmail.com>
2021-11-03 18:20:01 +0900

    update: token module message

--- src/screens/transaction_details/components/msg/token/mint/index.tsx
+++ src/screens/transaction_details/components/msg/token/mint/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import Trans from 'next-translate/Trans';
 import { Typography } from '@material-ui/core';
+import { Name } from '@components';
 import { MsgTokenMint } from '@models';
+import { useChainContext } from '@contexts';
 
 const TokenMint = (props: {
   message: MsgTokenMint;
 }) => {
+  const { findAddress } = useChainContext();
   const { message } = props;
+
+  const ownerAddress = findAddress(message.ownerAddress);
+  const ownerMoniker = ownerAddress ? ownerAddress?.moniker : message.ownerAddress;
+
 
   return (
     <Typography>
       <Trans
         i18nKey="message_contents:txTokenMintContent"
+        components={[
+          (
+            <Name
+              address={message.ownerAddress}
+              name={ownerMoniker}
+            />
+          ),
+        ]}
         values={{
             amount: message.amount,
             tokenID: message.tokenID,
