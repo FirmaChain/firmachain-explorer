979883ec14ed389c99adda192a3bc703343e21a9
chocoSprite <chocosprite21@gmail.com>
2022-05-03 10:31:13 +0900

    fix: transaction hash unify in uppercase letters

--- src/utils/go_to_page.ts
+++ src/utils/go_to_page.ts
@@ -6,7 +6,7 @@ export const BLOCK_DETAILS = (slot:string | number): string => `/blocks/${slot}`
 export const VALIDATOR_DETAILS = (address: string): string => `/validators/${address}`;
 export const VALIDATORS = '/validators';
 export const TRANSACTIONS = '/transactions';
-export const TRANSACTION_DETAILS = (tx: string): string => `/transactions/${tx}`;
+export const TRANSACTION_DETAILS = (tx: string): string => `/transactions/${tx.toUpperCase()}`;
 export const PROPOSALS = '/proposals';
 export const PROPOSAL_DETAILS = (id:string | number): string => `/proposals/${id}`;
 export const ACCOUNT_DETAILS = (address: string): string => `/accounts/${address}`;
