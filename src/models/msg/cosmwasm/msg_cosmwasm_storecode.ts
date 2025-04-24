bf25decde05d05ea4054ed040af58c50d73393d2
Luca Palla <l.palla1995@gmail.com>
2025-04-24 18:13:07 +0200

    Fix getWithdrawalAmount, add getCodeId, getInstantiateCodeId

--- src/models/msg/cosmwasm/msg_cosmwasm_storecode.ts
+++ src/models/msg/cosmwasm/msg_cosmwasm_storecode.ts
@@ -1,30 +1,47 @@
+import * as R from 'ramda';
+import { chainConfig } from '@configs';
+import { formatToken } from '@utils/format_token';
 import { Categories } from '../types';
 
 class MsgCosmwasmStoreCode {
-    public category: Categories;
-    public type: string;
-    public json: any;
+  public category: Categories;
+  public type: string;
+  public json: any;
 
-    public ownerAddress: string;
-    public codeId: string;
+  public ownerAddress: string;
+  public codeId: string;
 
-    constructor(payload: any) {
-      this.category = 'nft';
-      this.type = payload.type;
-      this.json = payload.json;
+  constructor(payload: any) {
+    this.category = 'nft';
+    this.type = payload.type;
+    this.json = payload.json;
 
-      this.ownerAddress = payload.ownerAddress;
-      this.codeId = payload.codeId;
+    this.ownerAddress = payload.ownerAddress;
+    this.codeId = payload.codeId;
+  }
+  static getCodeId(events: any) {
+    if (events === null) {
+      return 'Unknown';
     }
+    const WasmEvent = events.find(event =>
+      event.type === 'store_code' &&
+      event.attributes?.some(attr => attr.key === 'code_id')
+    );
+  
+    const codeIdAttr = WasmEvent?.attributes?.find(attr => attr.key === 'code_id');
+    const codeId = codeIdAttr?.value || 'Unknown'; 
+    return codeId
+  }
 
-    static fromJson(json: any, log: any) {
-      return new MsgCosmwasmStoreCode({
-        json,
-        type: json['@type'],
-        ownerAddress: json.sender,
-        codeId: log ? log.events[1].attributes[1].value : 'UNKNWON',
-      });
-    }
+  static fromJson(json: any, events: any) {
+    const codeId = this.getCodeId(events);
+    return new MsgCosmwasmStoreCode({
+      json,
+      type: json['@type'],
+      ownerAddress: json.sender,
+      codeId,
+    });
+  }
 }
 
 export default MsgCosmwasmStoreCode;
