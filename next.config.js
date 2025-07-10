3f249662aa014492bb69f9d24c853505c6d6e8cd
thismars <yhklip990@gmail.com>
2025-07-10 18:30:40 +0900

    feat: optimize webpack configuration for development environment and add experimental features for improved performance

--- next.config.js
+++ next.config.js
@@ -2,11 +2,24 @@ const nextTranslate = require('next-translate');
 
 module.exports = nextTranslate({
   poweredByHeader: false,
-  webpack: (config) => {
+  webpack: (config, { dev, isServer }) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
+    // Optimizing the Development Environment
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+
     return config;
+  },
+  // Experimental Features (Improved Performance)
+  experimental: {
+    esmExternals: false,
   },
 });
