424740cdf34f39f8b601cc6ebeb88a9e4c34c695
Larrykim <larry@firmainnovation.com>
2026-04-24 15:00:37 +0900

    refactor: switch to @vitejs/plugin-react-swc for improved performance
    - Replaced @vitejs/plugin-react with @vitejs/plugin-react-swc in package.json and vite.config.ts to leverage SWC's faster compilation.
    - Updated analyzer mode in vite.config.ts to 'server' for better analysis during development.
    - Cleaned up yarn.lock to reflect the changes in dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 import { analyzer } from 'vite-bundle-analyzer';
 import svgr from 'vite-plugin-svgr';
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => {
             svgr(),
             tsconfigPaths(),
             analyzer({
-                analyzerMode: 'static',
+                analyzerMode: 'server',
                 openAnalyzer: true
             })
         ],
