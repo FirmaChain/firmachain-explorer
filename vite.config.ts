c20ba2e248c93cdd60e5936d969dd79b2168fa47
Larrykim <larry@firmainnovation.com>
2026-05-06 20:00:30 +0900

    chore: remove vite-tsconfig-paths dependency and update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 import { analyzer } from 'vite-bundle-analyzer';
 import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => {
     const { PORT } = loadEnv(mode, process.cwd(), '');
@@ -11,7 +10,6 @@ export default defineConfig(({ mode }) => {
         plugins: [
             react(),
             svgr(),
-            tsconfigPaths(),
             analyzer({
                 analyzerMode: 'server',
                 openAnalyzer: true
@@ -26,6 +24,7 @@ export default defineConfig(({ mode }) => {
             sourcemap: false
         },
         resolve: {
+            tsconfigPaths: true,
             alias: {
                 '@mui/styled-engine': '@mui/styled-engine-sc'
             }
